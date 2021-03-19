5eaedcd824af06b32d6bb2b8fdebffda7180bc1e
Robert Forkel <xrotwang@googlemail.com>
2021-03-19 12:58:09 +0100

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytular',
-    version='0.1.0',
+    version='0.1.1.dev0',
     license='Apache 2.0',
     description='TuLaR curation library',
     long_description=open('README.md').read(),
